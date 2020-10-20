86ade03f247595cc61d2ee1f21cc5023ec020874
202005_kyh <linkan.kim@gmail.com>
2020-10-20 12:56:48 +0900

    20201020 12:55 SessionListener SessionAttributeListener
    세션과 리스너를 이용한 로그아웃 기능 구현

---
+++ src/main/java/kr/or/ddit/logout/LogoutServlet.java
@@ -0,0 +1,53 @@
+package kr.or.ddit.logout;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kr.or.ddit.member.model.MemberVO;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(LogoutServlet.class);
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		MemberVO memberVo = (MemberVO)request.getSession().getAttribute("S_MEMBER");
+		
+		logger.debug(memberVo.toString());
+		
+		request.getSession().invalidate();
+//		response.sendRedirect(request.getContextPath()+"/login.jsp");
+		request.getRequestDispatcher("/login").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
